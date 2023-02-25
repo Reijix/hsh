3f11a217391b53ea2ee5098ff9eb010387978be8
Leon Vatthauer <leon.vatthauer@fau.de>
2023-02-25 16:05:06 +0100

    Added background jobs and changed data structures

--- app/Main.hs
+++ app/Main.hs
@@ -1,59 +1,113 @@
 module Main where
 
+import Control.Monad.State
+import Data.Maybe (fromMaybe)
 import System.Directory
 import System.IO
 import System.Process
+import System.Process.Internals (ProcessHandle (ProcessHandle))
+
+data ShellState = ShellState
+  { history :: [String],
+    processes :: [(ProcessHandle, String)],
+    workingDir :: FilePath
+  }
+
+type ShellMonad = StateT ShellState IO
 
 -- entry point
 main :: IO ()
 main = do
   putStrLn "Welcome to the Haskell SHell!"
-  interactWithCwd runner
+  cwd <- getCurrentDirectory
+  evalStateT runner (ShellState [] [] cwd)
 
 -- commands
-cd :: [String] -> IO ()
-cd [dir] = setCurrentDirectory dir
-cd _ = putStrLn "Usage: cd <path>"
+cd :: [String] -> ShellMonad ()
+cd [dir] = do
+  liftIO $ setCurrentDirectory dir
+  cwd <- liftIO getCurrentDirectory
+  modify (\state -> state {workingDir = cwd})
+cd _ = liftIO $ putStrLn "Usage: cd <path>"
 
--- handles a single line from stdin
--- it checks whether we should execute an inbuilt function like 'cd'
--- or a normal program like 'ls'
-runner :: String -> IO ()
-runner str = dispatch (ownSplit ' ' str)
+jobs :: [String] -> ShellMonad ()
+jobs [] = do
+  jobs <- gets processes
+  liftIO $ mapM_ printJob jobs
   where
-    dispatch :: [String] -> IO ()
-    dispatch [] = return ()
-    dispatch ("cd" : args) = cd args
-    dispatch xs = executeProgram xs
+    printJob :: (ProcessHandle, String) -> IO ()
+    printJob (proc, cmdLine) = do
+      pidM <- getPid proc
+      let pid = fromMaybe (-1) pidM
+      putStrLn $ show pid ++ ": " ++ cmdLine
+jobs _ = liftIO $ putStrLn "Usage: jobs"
 
 -- splits a string on a given delimeter (on each occurrence of delim)
-ownSplit :: Char -> [Char] -> [[Char]]
-ownSplit delim xs = splitHelper delim xs []
+parse :: Char -> [Char] -> ([[Char]], Bool)
+parse delim xs = splitHelper delim xs []
   where
-    splitHelper :: Char -> [Char] -> [Char] -> [[Char]]
-    splitHelper delim [] [] = []
-    splitHelper delim [] drag = [drag]
+    splitHelper :: Char -> [Char] -> [Char] -> ([[Char]], Bool)
+    splitHelper delim [] [] = ([], False)
+    splitHelper delim [] drag = ([drag], False)
+    splitHelper delim "&" [] = ([], True)
+    splitHelper delim "&" drag = ([drag], True)
     splitHelper delim (x : xs) [] | delim == x = splitHelper delim xs []
-    splitHelper delim (x : xs) drag | delim == x = drag : splitHelper delim xs []
+    splitHelper delim (x : xs) drag | delim == x = (drag : strs, background)
+      where
+        (strs, background) = splitHelper delim xs []
     splitHelper delim (x : xs) drag = splitHelper delim xs (drag ++ [x])
 
 -- executes a program, takes in the whole commandline (including the programs name at cmdline[0])
-executeProgram :: [String] -> IO ()
+executeProgram :: [String] -> ShellMonad ()
 executeProgram [] = return ()
 executeProgram (prog : args) = do
-  (_, _, _, childHandle) <- createProcess cp
-  waitForProcess childHandle
+  (_, _, _, childHandle) <- liftIO $ createProcess cp
+  liftIO $ waitForProcess childHandle
   return ()
   where
     cp = proc prog args
 
+-- executes a program as background task, takes in the whole commandline (including the programs name at cmdline[0])
+executeProgramBackground :: [String] -> ShellMonad ()
+executeProgramBackground [] = liftIO $ putStrLn "hsh: '&' without command is invalid!"
+executeProgramBackground (prog : args) = do
+  (_, _, _, childHandle) <- liftIO $ createProcess cp
+  procs <- gets processes
+  modify (\state -> state {processes = (childHandle, unwords (prog : args)) : procs})
+  where
+    cp = proc prog args
+
 -- like preludes 'interact' but prints the CWD and takes a function that does IO itself
-interactWithCwd :: (String -> IO ()) -> IO ()
-interactWithCwd fun = printCwd >> getLine >>= fun >> interactWithCwd fun
+runner :: ShellMonad ()
+runner = do
+  checkJobs
+  printCwd
+  str <- liftIO getLine
+  dispatch $ parse ' ' str
+  runner
+  where
+    dispatch :: ([String], Bool) -> ShellMonad ()
+    dispatch ("cd" : args, _) = cd args
+    dispatch ("jobs" : args, _) = jobs args
+    dispatch (xs, True) = executeProgramBackground xs
+    dispatch (xs, False) = executeProgram xs
 
 -- prints the CWD in shell typical format
-printCwd :: IO ()
+printCwd :: ShellMonad ()
 printCwd = do
-  cwd <- getCurrentDirectory
-  putStr $ cwd ++ " > "
-  hFlush stdout
+  cwd <- gets workingDir
+  liftIO $ putStr $ cwd ++ " > "
+  liftIO $ hFlush stdout
+
+checkJobs :: ShellMonad ()
+checkJobs = do
+  jobs <- gets processes
+  newJobs <- liftIO $ filterM filterProcess jobs
+  modify (\state -> state {processes = newJobs})
+  where
+    filterProcess :: (ProcessHandle, String) -> IO Bool
+    filterProcess (proc, cmdline) = do
+      ecM <- getProcessExitCode proc
+      case ecM of
+        Nothing -> return True
+        Just exitCode -> putStrLn ("[" ++ cmdline ++ "] exited with code " ++ show exitCode) >> return False
